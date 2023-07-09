33ed11921a61fd8995e8a60a82773903a1d5406f
Lawal Abdul-Maatin <haydelawal@gmail.com>
2023-07-09 19:00:22 +0100

    1/2 second bug that shows empty data screen even if db isn't empty

--- app/src/main/java/com/example/todo_compose_ss/ui/viewmodel/MySharedViewModel.kt
+++ app/src/main/java/com/example/todo_compose_ss/ui/viewmodel/MySharedViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todo_compose_ss.data.models.TodoTask
 import com.example.todo_compose_ss.data.repositories.TodoRepository
+import com.example.todo_compose_ss.utils.RequestState
 import com.example.todo_compose_ss.utils.SearchAppBarState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -23,37 +24,23 @@ class MySharedViewModel @Inject constructor(
         mutableStateOf(SearchAppBarState.CLOSED)
     val searchTextState: MutableState<String> = mutableStateOf("")
 
-    private val _allTasks = MutableStateFlow<List<TodoTask>>(emptyList())
-    val allTasks: StateFlow<List<TodoTask>> = _allTasks
 
-    fun getAllTasks(){
-        viewModelScope.launch {
-            repository.getAllTasks.collect {
-                _allTasks.value = it
+    private val _allTasks =
+        MutableStateFlow<RequestState<List<TodoTask>>>(RequestState.Idle)
+    val allTasks: StateFlow<RequestState<List<TodoTask>>> = _allTasks
+
+
+    fun getAllTasks() {
+        _allTasks.value = RequestState.Loading
+        try {
+            viewModelScope.launch {
+                repository.getAllTasks.collect {
+                    _allTasks.value = RequestState.Success(it)
+                }
             }
+        } catch (e: Exception) {
+            _allTasks.value = RequestState.Error(e)
         }
     }
-
-
-//    val searchAppBarState: MutableState<SearchAppBarState> =
-//        mutableStateOf(SearchAppBarState.CLOSED)
-//    val searchTextState: MutableState<String> = mutableStateOf("")
-//
-//    private val _allTasks =
-//        MutableStateFlow<RequestState<List<ToDoTask>>>(RequestState.Idle)
-//    val allTasks: StateFlow<RequestState<List<ToDoTask>>> = _allTasks
-//
-//    fun getAllTasks() {
-//        _allTasks.value = RequestState.Loading
-//        try {
-//            viewModelScope.launch {
-//                repository.getAllTasks.collect {
-//                    _allTasks.value = RequestState.Success(it)
-//                }
-//            }
-//        } catch (e: Exception) {
-//            _allTasks.value = RequestState.Error(e)
-//        }
-//    }
 
 }
---
+++ app/src/main/java/com/example/todo_compose_ss/utils/RequestState.kt
@@ -0,0 +1,8 @@
+package com.example.todo_compose_ss.utils
+
+sealed class RequestState<out T> {
+    object Idle : RequestState<Nothing>()
+    object Loading : RequestState<Nothing>()
+    data class Success<T>(val data: T) : RequestState<T>()
+    data class Error(val error: Throwable) : RequestState<Nothing>()
+}
